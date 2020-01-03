ee8cf3c8ef15de9f3549fe6924c9879ecf04e962
aidai.abakirova <aidai.abakirova@gmail.com>
2020-01-03 11:50:18 -0600

    Fixed all 1-3-20

---
+++ src/Man.java
@@ -0,0 +1,10 @@
+public class Man {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int sum = a + b;
+        int sub = a - b;
+        System.out.println(sum);
+        System.out.println(sub);
+    }
+}
--- src/ReversedNumber.java
+++ src/ReversedNumber.java
@@ -1,10 +1,10 @@
 public class ReversedNumber {
     public static void main(String[] args) {
         int n = 672;
-        int reversed =0;
-        int digit = 0;
+        int reversed = 0;
+        int digit;
 
-        while(n!=0){
+        while (n != 0) {
             digit = n % 10;
             reversed = (reversed * 10) + digit;
             n = n / 10;
